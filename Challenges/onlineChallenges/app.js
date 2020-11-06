829c79488e9f254fb4eddf5ca9d29c6219624271
Levin <levinbatallones@yahoo.com>
2020-11-05 16:18:06 -0800

    [onlineChallenges] add our research challenges

---
+++ Challenges/onlineChallenges/app.js
@@ -0,0 +1,57 @@
+const f = n => {
+    if (n <= 1) {
+        return 1
+    } else {
+        return n * f(n - 1)
+    }
+}
+f(4)
+// output 24
+
+function f(x) {
+    x *= 2;
+    return function(y) {
+     y *= x;
+        return function(z) {
+            return z * y
+        }
+    }
+}
+
+f(3)(4)(5)
+// output 120
+
+
+function makeAdder(x) {
+    return function (n) {
+        return x + n
+    }
+}
+
+let add5 = makeAdder(5);
+let add8 = makeAdder(8);
+let add20 = makeAdder(20);
+console.log(add5(10))
+console.log(add8(6))
+console.log(add20(6))
+// output(add5(10) === 15)
+// output(add8(6) === 14)
+// output(add20(6) === 16)
+
+
+function foo () {
+
+    function bar () {
+        setTimeout(
+        () => console.log('Curly'),1000);
+    }
+    console.log('Larry')
+    return bar;
+}
+
+let x = foo();
+x()
+console.log('Moe')
+// output "Larry" "Moe" "Curly"
+
+

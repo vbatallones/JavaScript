1d6b58cc43aa0e1669ebe1b29208a9fa9d60ff88
Levin <vbatallones.xvi@gmail.com>
2020-11-15 22:27:23 -0800

    change function name and convert it into ES6 function

--- Challenges/onlineChallenges/app.js
+++ Challenges/onlineChallenges/app.js
@@ -1,24 +1,24 @@
-const f = n => {
-    if (n <= 1) {
-        return 1
-    } else {
-        return n * f(n - 1)
-    }
-}
-f(4)
+// const f = n => {
+//     if (n <= 1) {
+//         return 1
+//     } else {
+//         return n * f(n - 1)
+//     }
+// }
+// f(4)
 // output 24
 
-function f(x) {
+const multiplyFunctions = (x) => {
     x *= 2;
-    return function(y) {
-     y *= x;
-        return function(z) {
-            return z * y
+        return function(y) {
+            y *= x;
+            return function(z) {
+                return z * y
+            }
         }
-    }
 }
 
-f(3)(4)(5)
+multiplyFunctions(3)(4)(5)
 // output 120
 
 
@@ -34,24 +34,24 @@ let add20 = makeAdder(20);
 console.log(add5(10))
 console.log(add8(6))
 console.log(add20(6))
-// output(add5(10) === 15)
-// output(add8(6) === 14)
-// output(add20(6) === 16)
+output(add5(10) === 15)
+output(add8(6) === 14)
+output(add20(6) === 16)
 
 
-function foo () {
+// function foo () {
 
-    function bar () {
-        setTimeout(
-        () => console.log('Curly'),1000);
-    }
-    console.log('Larry')
-    return bar;
-}
+//     function bar () {
+//         setTimeout(
+//         () => console.log('Curly'),1000);
+//     }
+//     console.log('Larry')
+//     return bar;
+// }
 
-let x = foo();
-x()
-console.log('Moe')
+// let x = foo();
+// x()
+// console.log('Moe')
 // output "Larry" "Moe" "Curly"
 
 

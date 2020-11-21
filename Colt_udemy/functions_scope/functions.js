c0989a5b408f9759f8d372131b0d1448b30f5bd2
Levin <vbatallones.xvi@gmail.com>
2020-11-20 23:57:48 -0800

    understanding block scope

--- Colt_udemy/functions_scope/functions.js
+++ Colt_udemy/functions_scope/functions.js
@@ -1,22 +1,42 @@
 // Functions Scope
 
-let animal = 'Lion'
+// let animal = 'Lion'
 
-function myPet(pet) {
-    let animal = pet
-    console.log(animal) 
+// function myPet(pet) {
+//     let animal = pet
+//     console.log(animal) 
+// }
+// myPet('Cat') // ---> Cat
+// console.log(animal) // ----> Lion
+
+
+// const phone = 'Iphone 12'
+
+// function myPhone() {
+//     phone = 'Samsung Galaxy 20'
+//     console.log(phone) 
+// }
+// myPhone() // ---> it will give an error because of a const variable declaration.
+
+
+// Block scope
+
+let money = 1000
+if (money > 0) {
+    const people = 'Levin'
+    let msg = 'You have money'
+    // inside the block it will work. 
+    console.log(people) //--> Levin
+    console.log(msg) // --> You have money
 }
-myPet('Cat') // ---> Cat
-console.log(animal) // ----> Lion
+
+console.log(money) //----> will give us 1000
+console.log(people) // ---> will give an error because it was inside a block 
+console.log(msg) // ---> will give an error because it was inside a block 
 
 
-const phone = 'Iphone 12'
-
-function myPhone() {
-    phone = 'Samsung Galaxy 20'
-    console.log(phone) 
-}
-myPhone() // ---> it will give an error because of a const variable declaration.
+//  Lexical Scope
+// an inner function nested inside of parent function
 
 
 

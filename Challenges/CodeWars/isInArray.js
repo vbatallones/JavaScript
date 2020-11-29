3e60e0a9309b6906469facd9e225fdc114d44200
Levin <vbatallones.xvi@gmail.com>
2020-11-28 23:20:24 -0800

    making it a ES6 function single line

--- Challenges/CodeWars/isInArray.js
+++ Challenges/CodeWars/isInArray.js
@@ -2,13 +2,11 @@
 return a sorted array in lexicographical order 
 of the strings of a1 which are substrings of strings of a2 */
 
-function inArray(arr1, arr2) {
+const inArray = (arr1, arr2) => arr1.filter(val => arr2.join(' ').includes(val)).sort()
 
-    return arr1.filter(val => arr2.join(' ').includes(val)).sort()
 
-}
 
 
 a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
 a1 = ["xyz", "live", "strong"]
-inArray(a1, a2) // [ 'live', 'strong' ]
+console.log(inArray(a1, a2)) // [ 'live', 'strong' ]

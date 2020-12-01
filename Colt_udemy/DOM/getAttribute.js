834e1c221e1568b40bc668762ae29adca0e64e61
Levin <vbatallones.xvi@gmail.com>
2020-11-30 17:12:58 -0800

    get attribute method in alink

--- Colt_udemy/DOM/getAttribute.js
+++ Colt_udemy/DOM/getAttribute.js
@@ -10,3 +10,15 @@ document.querySelector('#notContainer').id = 'container'
 // notice that we now need to target the id notContainer
 // change it back to id container
 
+
+// Accessing the link attribute
+
+// the file is already prepend unlike
+document.querySelector('a').href
+"file:///Users/levinbatallones/Desktop/Javascript/JS/JavaScript/Colt_udemy/DOM/www.levin.com"
+
+// if we use the getAttribute method it will give just the link
+const aLink = document.querySelector('a')
+aLink.getAttribute('href')
+// www.levin.com
+

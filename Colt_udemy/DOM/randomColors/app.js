20d705875fc392fca2c6deb20bb1eaad1116ac5e
Levin <vbatallones.xvi@gmail.com>
2020-12-02 23:02:13 -0800

    remove conditional

--- Colt_udemy/DOM/randomColors/app.js
+++ Colt_udemy/DOM/randomColors/app.js
@@ -10,14 +10,8 @@ function randomColor() {
     const r = Math.floor(Math.random() * 255)
     const g = Math.floor(Math.random() * 255)
     const b = Math.floor(Math.random() * 255)
-    let rgb = r + g + b
-    if (rgb < 50) {
-        h1.style.color = 'white' 
         return `rgb(${r}, ${g}, ${b})`
-    } else {
-        h1.style.color = 'black'
-        return `rgb(${r}, ${g}, ${b})`
-    }
+
 }
 
 button.addEventListener('click', changeColor)

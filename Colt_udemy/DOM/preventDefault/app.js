aa6f7e6da3ad330be7dbf2fd05ea33bdf1abd36d
Levin <vbatallones.xvi@gmail.com>
2020-12-04 16:25:14 -0800

    tackle about e.preventDefault and event listeners to post comment and name

---
+++ Colt_udemy/DOM/preventDefault/app.js
@@ -0,0 +1,24 @@
+const happyForm = document.querySelector('#happyForm')
+const commentsList = document.querySelector('#comments')
+
+happyForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const username = happyForm.elements.name;
+    const comment = happyForm.elements.comment;
+    
+    newComment(username.value, comment.value)
+    username.value = '';
+    comment.value = '';
+
+})
+
+const newComment = (username, comment) => {
+
+    const newComment = document.createElement('li');
+    const boldName = document.createElement('b');
+
+    boldName.append(username)
+    newComment.append(boldName)
+    newComment.append(` - ${comment}`)
+    commentsList.append(newComment)
+}

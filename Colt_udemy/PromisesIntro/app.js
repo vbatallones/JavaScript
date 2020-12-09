33dbba0cf2243ceb4a6dc22f8f7f17d4bc387cc1
Levin <vbatallones.xvi@gmail.com>
2020-12-09 11:26:08 -0800

    new folder for promises introduction

---
+++ Colt_udemy/PromisesIntro/app.js
@@ -0,0 +1,33 @@
+// THE CALLBACK VERSION
+const fakeRequestCallback = (url, success, failure) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+        if (delay > 4000) {
+            failure('Connection Timeout :(')
+        } else {
+            success(`Here is your fake data from ${url}`)
+        }
+    }, delay)
+}
+// THE PROMISE VERSION 
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+
+
+makeRequest(() => {
+
+}, 
+() => {
+    
+})

7d977444715049512d285da9f462880221b59eb6
Levin <vbatallones.xvi@gmail.com>
2020-12-09 15:31:46 -0800

    using async and  await to a request promise

--- Colt_udemy/AsyncAndAwait/app.js
+++ Colt_udemy/AsyncAndAwait/app.js
@@ -6,12 +6,27 @@ const firstAndLastname = async (firstName, lastName) => {
     throw 'Check your last name or first name'
 }
 
-firstAndLastname('Levin', 'Batallones')
-.then(data => {
-    console.log('Hello')
-    console.log(data)
-}) 
-.catch(err => {
-    console.log('Error')
-    console.log(err)
-})
+// firstAndLastname('Levin', 'Batallones')
+// .then(data => {
+//     console.log('Hello')
+//     console.log(data)
+// }) 
+// .catch(err => {
+//     console.log('Error')
+//     console.log(err)
+// })
+
+// ------------------------------------------
+
+//AWAIT
+
+async function makeRequest() {
+    try {
+        let myData = await firstAndLastname('Levin', 'Batallones');
+        console.log(myData)
+    } catch(e) {
+        console.log("There's an error")
+    }
+}
+
+makeRequest()

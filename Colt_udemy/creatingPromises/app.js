a73764894ca819b24795496a3d110aed5557af46
Levin <vbatallones.xvi@gmail.com>
2020-12-09 14:57:34 -0800

    setting the background color of the body to a promise request and using implicit return

--- Colt_udemy/creatingPromises/app.js
+++ Colt_udemy/creatingPromises/app.js
@@ -10,12 +10,28 @@ const request = (url) => {
         }, 3000)
     })
 }
+// promise request
 
-request('myData')
-.then((data) => {
-    console.log('Data success in here')
-    console.log(data)
-})
-.catch((err) => {
-    console.log(err)
-})
+// request('myData')
+// .then((data) => {
+//     console.log('Data success in here')
+//     console.log(data)
+// })
+// .catch((err) => {
+//     console.log(err)
+// })
+
+const delayedColor = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color
+            resolve() // this is a callBack that will run the next function
+        }, delay)
+    })
+}
+
+delayedColor('red', 1000)
+// using implicit return
+.then(() => delayedColor('orange', 1000))
+.then(() => delayedColor('yellow', 1000))
+.then(() => delayedColor('green', 1000))

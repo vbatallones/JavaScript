2fb0803848a6db453f9596306b79835a6bb62bd8
Levin <vbatallones.xvi@gmail.com>
2020-12-09 18:29:25 -0800

    clear console logs

--- Colt_udemy/AsyncAndAwait/app.js
+++ Colt_udemy/AsyncAndAwait/app.js
@@ -6,15 +6,15 @@ const firstAndLastname = async (firstName, lastName) => {
     throw 'Check your last name or first name'
 }
 
-// firstAndLastname('Levin', 'Batallones')
-// .then(data => {
-//     console.log('Hello')
-//     console.log(data)
-// }) 
-// .catch(err => {
-//     console.log('Error')
-//     console.log(err)
-// })
+firstAndLastname('Levin', 'Batallones')
+.then(data => {
+    console.log('Hello')
+    console.log(data)
+}) 
+.catch(err => {
+    console.log('Error')
+    console.log(err)
+})
 
 // ------------------------------------------
 

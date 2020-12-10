0b6773d1309451ae6b2c3c2412d9d13f16024029
Levin <vbatallones.xvi@gmail.com>
2020-12-10 15:16:31 -0800

    add a fetch request for api and did the async and await for shorter example

--- Colt_udemy/makingRequest/app.js
+++ Colt_udemy/makingRequest/app.js
@@ -1,3 +1,6 @@
+// XMLHTTP request
+// old way of doing it
+
 const req = new XMLHttpRequest();
 
 req.onload = function () {
@@ -13,3 +16,31 @@ req.onerror = function () {
 
 req.open('GET', 'https://api.cryptonator.com/api/ticker/btc-usd')
 req.send()
+
+
+// fetch
+
+fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+.then(res => {
+    console.log('RESPONSE, waiting to parse', res)
+    return res.json()
+})
+.then(data => {
+    console.log('DATA PARSE')
+    console.log(data.ticker.price)
+})
+.catch(err => {
+    console.log('ERROR', err)
+})
+
+// Async and await
+
+const fetchBitcoin = async() => {
+    try {
+        const req = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+        const data = await req.json()
+        console.log(data.ticker.price)
+    } catch (e) {
+        console.log('ERROR', e)
+    }
+}

1e9e979d9a70a3dbb97431226d11849689d81d3d
Levin <vbatallones.xvi@gmail.com>
2020-12-17 18:08:17 -0800

    uneccessary file

--- Colt_udemy/node_intro/args.js
+++
@@ -1,2 +0,0 @@
-console.log('hello from args file')
-console.log(process.argv)
--- Colt_udemy/node_intro/jokester/index.js
+++
@@ -1,7 +0,0 @@
-const giveMeAJoke = require('give-me-a-joke');
-const colors = require('colors')
-const cowsay = require('cowsay')
-
-giveMeAJoke.getRandomDadJoke (function(joke) {
-    console.log(joke.rainbow);
-});

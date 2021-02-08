b26d321d883ffe03fdee382ecc475e646453e6f3
Levin <vbatallones.xvi@gmail.com>
2021-02-07 22:06:02 -0800

    add a function toggle for the switch that will make toggle effect on dark mode and bright mode

--- Animations/background-animation/app.js
+++ Animations/background-animation/app.js
@@ -8,16 +8,24 @@
 //     "#74ff1d"
 // ]
 
+
+const section = document.querySelector('section');
+const toggleButt = document.querySelector('.toggle'); 
+const bright = document.querySelector('.bright');
+const cloud1 = document.querySelector('.cloud1')
+const cloud2 = document.querySelector('.cloud2')
+const cloud3 = document.querySelector('.cloud3')
+
+
 const createStar = () => {
-    const section = document.querySelector('section');
-    const star = document.createElement('span');
+    let star = document.createElement('span');
 
     let size = Math.random() * 10;
 
     star.style.width = 10 * size + "px";
     star.style.height = 10 * size + "px";
     // added the shadow effect on the parent element
-    section.style.filter = 'drop-shadow(3rem 3rem 3rem yellow)'
+    // section.style.filter = 'drop-shadow(3rem 3rem 3rem yellow)'
     // Star using CSS clip path
     star.style.clipPath = 'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)'
     star.style.top = Math.random() * innerHeight + 'px';
@@ -30,9 +38,28 @@ const createStar = () => {
     section.appendChild(star)
 
     setTimeout(() => {
-        square.remove()
-    }, 5000);
+        star.remove()
+    }, 2000);
+}
+
+const toggleMe = () => {
+    if (section.style.display === "none") {
+        section.style.display = "block";
+        document.body.classList.remove("bright")
+        cloud1.style.position = "static"
+        cloud2.style.position = "static"
+        cloud3.style.position = "static"
+    } else {
+        document.body.classList.toggle("bright")
+        cloud1.style.position = "absolute"
+        cloud2.style.position = "absolute"
+        cloud3.style.position = "absolute"
+        section.style.display = "none";
+    }
+    
 }
 
 
 setInterval(createStar, 150);
+
+toggleButt.addEventListener('change', toggleMe)

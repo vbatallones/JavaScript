24d6a5dda58e6ebaa673c2145fe264cb02c0a7d0
Levin <vbatallones.xvi@gmail.com>
2021-02-13 11:11:40 -0800

    add another solution if you have a punctuation mark, using hashmap

--- Challenges/CodeWars/pigLatin.js
+++ Challenges/CodeWars/pigLatin.js
@@ -1,15 +1,19 @@
-function pigIt(str) {
+const pigIt = (str) => {
     //Code here
-    let patt = /\b\w/g;
     let result = []
-
+    let word = 'ay'
+    let exclude = { '!': 1, '?': 1 };
     for (let char of str.split(' ')) {
         let newChar = char.slice(1)
-        result.push(`${newChar}${char.match(patt).concat('ay').join('')}`)
+        if (!exclude[char]) {
+            result.push(`${newChar}${char[0]}${word}`)
+        } else if (exclude[char]) {
+            result.push(`${char}`)
+        }
     }
     return result.join(' ')
 }
 
 
 console.log(pigIt('Pig latin is cool')) //'igPay atinlay siay oolcay'
-console.log(pigIt('This is my string')) //'hisTay siay ymay tringsay'
+console.log(pigIt('This is my string !')) //'hisTay siay ymay tringsay'

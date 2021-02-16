1588403b1e590ec5b77b35837d4a6f5a19d46d93
Levin <vbatallones.xvi@gmail.com>
2021-02-16 01:49:28 -0800

    change the name of the function

--- Challenges/onlineChallenges/average.js
+++ Challenges/onlineChallenges/average.js
@@ -3,7 +3,7 @@
 // Use Array.prototype.reduce() to add each value to an accumulator, initialized with a value of 0, divide by the length of the array.
 
 
-const average = (...nums) => nums.reduce((acc, val) => acc + val, 0) / nums.length;
+const averageJoe = (...nums) => nums.reduce((acc, val) => acc + val, 0) / nums.length;
 
-console.log(average(...[1, 2, 3])); // 2
-console.log(average(3, 4, 5)); // 4
+console.log(averageJoe(...[1, 2, 3])); // 2
+console.log(averageJoe(3, 4, 5)); // 4

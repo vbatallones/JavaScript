e1d714608dc9f4df5dfb05619486f501c91e99b9
Levin <vbatallones.xvi@gmail.com>
2021-02-16 00:08:36 -0800

    add a different value for testing purposes

--- Challenges/onlineChallenges/average.js
+++ Challenges/onlineChallenges/average.js
@@ -5,5 +5,5 @@
 
 const average = (...nums) => nums.reduce((acc, val) => acc + val, 0) / nums.length;
 
-average(...[1, 2, 3]); // 2
-average(1, 2, 3); // 2
+console.log(average(...[1, 2, 3])); // 2
+console.log(average(3, 4, 5)); // 4

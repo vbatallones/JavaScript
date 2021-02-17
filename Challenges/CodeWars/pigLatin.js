a714bce3a73bd19afe493effed99ce497f45262c
Levin <vbatallones.xvi@gmail.com>
2021-02-17 12:10:50 -0800

    revise the solution for the pigLatin problem

--- Challenges/CodeWars/pigLatin.js
+++ Challenges/CodeWars/pigLatin.js
@@ -16,4 +16,4 @@ const pigIt = (str) => {
 
 
 console.log(pigIt('Pig latin is cool')) //'igPay atinlay siay oolcay'
-console.log(pigIt('This is my string !')) //'hisTay siay ymay tringsay'
+console.log(pigIt('This is my string !')) //'hisTay siay ymay tringsay !'

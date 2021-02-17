36bbbca796771bd6da975787501ddada8dce020f
Levin <vbatallones.xvi@gmail.com>
2021-02-17 01:03:05 -0800

    add a ascending or descending order depending on the input, using the sort, reduce, and destructuring it

---
+++ Challenges/onlineChallenges/orderBy.js
@@ -0,0 +1,23 @@
+// Returns a sorted array of objects ordered by properties and orders.
+
+// Uses Array.prototype.sort(), Array.prototype.reduce() on the props array with a default value of 0,
+//  use array destructuring to swap the properties position depending on the order passed.
+//  If no orders array is passed it sort by 'asc' by default.
+
+
+const whatsYourOrder = (arr, props, orders) =>
+    [...arr].sort((a, b) =>
+        props.reduce((acc, prop, i) => {
+            if (acc === 0) {
+                const [p1, p2] = orders && orders[i] === 'desc' ? [b[prop], a[prop]] : [a[prop], b[prop]];
+                acc = p1 > p2 ? 1 : p1 < p2 ? -1 : 0;
+            }
+            return acc;
+        }, 0)
+    );
+
+
+const users = [{ name: 'fred', age: 48 }, { name: 'barney', age: 36 }, { name: 'fred', age: 40 }];
+
+console.log(whatsYourOrder(users, ['name', 'age'], ['asc', 'desc'])); // [{name: 'barney', age: 36}, {name: 'fred', age: 48}, {name: 'fred', age: 40}]
+console.log(whatsYourOrder(users, ['name', 'age'])); // [{name: 'barney', age: 36}, {name: 'fred', age: 40}, {name: 'fred', age: 48}]

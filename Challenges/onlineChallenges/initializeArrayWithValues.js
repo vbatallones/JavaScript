17acb4966771991edfd67394d1c5ff016873c724
Levin <vbatallones.xvi@gmail.com>
2021-03-02 16:26:57 -0800

    initialized and fills an array with the specified values.

---
+++ Challenges/onlineChallenges/initializeArrayWithValues.js
@@ -0,0 +1,12 @@
+// Initializes and fills an array with the specified values.
+
+// Use Array(n) to create an array of the desired length, fill(v) to fill it with the desired values.
+//  You can omit val to use a default value of 0.
+
+const initializeThisValues = (num, value = 0) => Array(num).fill(value);
+
+
+console.log(initializeThisValues(1, "I")) //[ 'I' ]
+console.log(initializeThisValues(4,"LOVE")) //[ 'LOVE', 'LOVE', 'LOVE', 'LOVE' ]
+console.log(initializeThisValues(3,"JavaScript")) //[ 'JavaScript', 'JavaScript', 'JavaScript' ]
+console.log(initializeThisValues(4,20)) //[ 20, 20, 20, 20 ]

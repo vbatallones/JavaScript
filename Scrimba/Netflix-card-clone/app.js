2395f25699da3fbf1ae3ff163c8d9b18bf19b2c0
Levin <vbatallones.xvi@gmail.com>
2021-06-03 23:57:40 -0700

    setting titles for the movies and making a function to render the names by manipulating the dom

---
+++ Scrimba/Netflix-card-clone/app.js
@@ -0,0 +1,17 @@
+const movies = {
+		title: [
+			'Lucifer',
+			'Nobody',
+			'I care a lot',
+			'Demon Slayer',
+			'Mortal Kombat',
+			'6 Underground',
+		],
+	}
+
+for (let movie in movies) {
+    console.log(movies.title)
+}
+
+
+const container = document.getElementsByClassName('card-container');
